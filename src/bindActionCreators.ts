5d2215026b5878f7ec2e54ba290d5ff6469b8ceb
Marko Skugor <marko.skugor.1@gmail.com>
2022-08-26 14:46:59 -0400

    SCC comments

--- src/bindActionCreators.ts
+++ src/bindActionCreators.ts
@@ -10,6 +10,15 @@ function bindActionCreator<A extends AnyAction = AnyAction>(
   actionCreator: ActionCreator<A>,
   dispatch: Dispatch
 ) {
+  /**
+   * #SCC
+   *
+   * Spread operator for function args to combine them in an array is nice and I'm
+   * making a mental note to use this. It's easy to forget all the cool things modern
+   * JS can do.
+   *
+   * - Marko Skugor
+   */
   return function (this: any, ...args: any[]) {
     return dispatch(actionCreator.apply(this, args))
   }
@@ -36,6 +45,20 @@ function bindActionCreator<A extends AnyAction = AnyAction>(
  * function as `actionCreators`, the return value will also be a single
  * function.
  */
+
+/**
+ * #SCC
+ * These overload signatures threw me for a loop. I didn't realize that this was possible,
+ * and had to stare at it for a while to understand what was going on. Almost universally
+ * at PGR we have the eslint import/export rule turned on which prevents multiple default
+ * exports in a file. I'm still not totally sure I understand all the typescript generics
+ * wizardry that's happening here.
+ *
+ * The eslint import/export rule for reference:
+ * https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/export.md
+ *
+ * - Marko Skugor
+*/
 export default function bindActionCreators<A, C extends ActionCreator<A>>(
   actionCreator: C,
   dispatch: Dispatch
@@ -64,6 +87,18 @@ export default function bindActionCreators(
   }
 
   if (typeof actionCreators !== 'object' || actionCreators === null) {
+    /**
+     * #SCC
+     *
+     * Another thing we avoid on my project via eslint is throw errors in our JS code regardless of if
+     * they're handled upstream or not. We use the functional/no-throw-statement rule to enforce this.
+     *
+     * Check here for reasoning:
+     * https://github.com/eslint-functional/eslint-plugin-functional/blob/main/docs/rules/no-throw-statement.md
+     *
+     * - Marko Skugor
+    */
+
     throw new Error(
       `bindActionCreators expected an object or a function, but instead received: '${kindOf(
         actionCreators
@@ -73,6 +108,22 @@ export default function bindActionCreators(
   }
 
   const boundActionCreators: ActionCreatorsMapObject = {}
+
+  /**
+     * #SCC
+     *
+     * I've always learned to avoid using for...in for iterating over object keys because
+     * it also iterates over all properties in that object's prototype chain. In this case there
+     * is an if statement that checks we are actually getting an action creator like we expect, but
+     * the safer way that I've always done this is to use Object.keys to get an array of all
+     * keys on the object and then just use a map or forEach or something. The for...in syntax
+     * is nice which is my guess as to why they're using it here, but I think there is something
+     * to be said about how nice it is to use array functions that enforce object immutability like
+     * map and reduce. It's also kind of interesting to see object mutation in the source code of a library
+     * that enforces state immutability. Maybe we don't need to be so dogmatic about it?
+     *
+     * - Marko Skugor
+    */
   for (const key in actionCreators) {
     const actionCreator = actionCreators[key]
     if (typeof actionCreator === 'function') {
